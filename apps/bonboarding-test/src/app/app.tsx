d655467495f952d3446b96829809a201ce389c23
Adam Karacsony <adam.karacsony+tech@kasa.com>
2022-09-07 11:17:54 +0200

    minimal error reproduction

--- apps/bonboarding-test/src/app/app.tsx
+++ apps/bonboarding-test/src/app/app.tsx
@@ -1,15 +1,30 @@
-import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
+import {useEffect} from 'react';
 
 export function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = 'https://go.bonboarding.com';
+    script.dataset['bbKey'] = 'mhdx0qvwbn';    
+
+    script.onload = () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore:next-line
+      window.Bonboarding.identify({ email: 'kasa-test@example.com' });
+    }
+    
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    }
+  }, []);
+
+
   return (
-    <StyledApp>
-      <NxWelcome title="bonboarding-test" />
-    </StyledApp>
+    <h1>
+      <span> Hello there 👋</span>
+    </h1>
   );
 }
 
